fd316c722b449d3d0861b3f309483974d0262875
said_sadu <sadullaev.1996@gmail.com>
2022-08-29 11:44:22 +0300

    решена 1 задача, но есть вопрос

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+double[,] arr = new double[5, 10];
+
+Random random = new Random();
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double [,]matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        arr[i, j] = random.NextDouble() * 100;
+        Console.Write("{0,6:F2} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+}
+
+FillArray(arr);
+//PrintArray(arr);
+
+//создал метод для вывода печати PrintArray, но при его активации дважды выводит массив -
+// в первом варианте округлен до 2-х знаков после запятой, 
+// а во втором варианте без округления
+//при его деактивации соответсвенно выыодит только один масси с числами
+//вопрос: обязательно ли создавать метод печати массива(PrintArray)?
